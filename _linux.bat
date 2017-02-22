0ed23174483de18ed1ca45cd59a9093099901dc5
Chris Rorden <rorden@mailbox.sc.edu>
2017-02-22 11:01:36 -0500

    Linux remove geometry shader from build

--- _linux.bat
+++ _linux.bat
@@ -1,7 +1,7 @@
 cd ~/surf-ice
 
-#1.) build OpenGL core version of surfice
 
+#compile Surfice to OpenGL 3.3 CORE
 
 #next lines obsolete: we use glcorearb.pas
 # the copy of glext that comes with freepascal 3.0 does not support geometry shaders
@@ -14,8 +14,11 @@ cp surfice ~/Surf_Ice/surfice
 
 
 
-#compile Surfice as 32-bit Carbon: OSX 10.6 OpenGL support (OpenGL2.1)
-cp ./optsCompatGeom.inc ./opts.inc
+#compile Surfice to OpenGL 2.1
+
+#next line not used: we will not use the geometry shaders
+# cp ./optsCompatGeom.inc ./opts.inc
+cp ./optsCompat.inc ./opts.inc
 
 rm -rf lib
 lazbuild -B surfice.lpr
@@ -23,8 +26,8 @@ cp surfice ~/Surf_Ice/surficeOld
 
 #clean up - remove modified glext so it does not interfere with other builds
 rm -rf lib
-cp ./optsCompat.inc ./opts.inc
-rm ./glext.pp
+#cp ./optsCompat.inc ./opts.inc
+#rm ./glext.pp
 
 cd ~
 zip -FSr surfice_linux.zip Surf_Ice
